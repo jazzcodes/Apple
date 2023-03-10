091de6097fb206dba4afc0ac25f11ab325d572e5
jazzcodes <jasleenbhambra01@gmail.com>
2023-03-10 12:36:45 +0530

    changes in js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ collapsibles.forEach((item) =>
 // define a function that sets min-height of my-element to window.innerHeight:
 
 const setHeight = () => {
-  document.getElementById("my-element").style.minHeight = window.innerHeight + "px"
+  document.getElementById("page").style.minHeight = window.innerHeight + "px"
 };
 
 // define mobile screen size:
