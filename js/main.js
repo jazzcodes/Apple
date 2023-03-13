d2907d293f1d18c569eb0126a7dfc05936892a01
jazzcodes <jasleenbhambra01@gmail.com>
2023-03-14 04:44:52 +0530

    js added

--- js/main.js
+++ js/main.js
@@ -5,22 +5,7 @@ collapsibles.forEach((item) =>
   })
 );
 
-// define a function that sets min-height of my-element to window.innerHeight:
-
-// const setHeight = () => {
-//   document.getElementById("page").style.height = window.innerHeight + "px"
-// };
-
-// // define mobile screen size:
-
-// let deviceWidth = window.matchMedia("(max-width: 414px)");
-
-// if (deviceWidth.matches) {
-// // set an event listener that detects when innerHeight changes:
-
-//   window.addEventListener("resize", setHeight);
-
-// // call the function once to set initial height:
-
-//   setHeight();
-// }
+(function () {
+  const el = document.getElementsByClassName('page');
+  el.style.height = window.innerHeight + 'px';
+})();
